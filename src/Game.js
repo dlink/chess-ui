9d94706dbff5c0ba6cd0d46cb4289bce748514fe
David Link <dlink@Davids-MBP.home>
2021-05-16 19:28:42 -0400

    checkboard covering, and square indicator dot added

--- src/Game.js
+++ src/Game.js
@@ -17,10 +17,22 @@ class Board extends React.Component {
     
     render() {
 	let rows = []
+	let value = null;
+	let isIndicated = null;
+	
 	for (let y = 1; y <= BOARD_HEIGHT; y++) {
+	    value = null;
+	    // only put pieces on these rows
+	    if ([1,2, 7, 8].includes(y)) {
+		value = 'P';
+	    }
 	    let squares = []
 	    for (let x = 1; x <= BOARD_WIDTH; x++) {
-		squares.push(<Square />)
+		isIndicated = false;
+		if (x == 6 && y == 6) {
+		    isIndicated = true;
+		}
+		squares.push(<Square value={value} isIndicated={isIndicated} />)
 	    }
 	    rows.push(squares)
 	}
@@ -35,8 +47,19 @@ class Board extends React.Component {
 }
 
 class Square extends React.Component {
+    constructor(props) {
+	super(props);
+	this.value = props.value;
+	this.isIndicated = props.isIndicated
+    }
     render() {
-	return (<div className='square'>P</div>);
+	const className = this.isIndicated ? 'dot active' : 'dot'
+	
+	return (
+		<div className='square square-indicator'>
+		{this.value}
+	        <span className={className} />
+	        </div>);
     }
 }
 
