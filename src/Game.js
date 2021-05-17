eb9b2bb3c2a3588a889e9cb0d5a87879c03cda14
David Link <dlink@Davids-MBP.home>
2021-05-16 21:36:24 -0400

    added click event handler

--- src/Game.js
+++ src/Game.js
@@ -14,25 +14,70 @@ class Game extends React.Component {
 }
 
 class Board extends React.Component {
-    
+
+    constructor(props) {
+	super(props);
+	this.handleClick.bind(this);
+
+	// inital board
+	let board = [];
+	let value = null;
+	for (let x = 1; x <= BOARD_WIDTH; x++) {
+	    board.push([])
+	    for (let y = 1; y <= BOARD_HEIGHT; y++) {;
+		let key = x + ':' + y;
+		board[board.length-1].push(
+		    {key: key,
+		     x: x,
+		     y: y,
+		     value: value});
+	    }
+	}
+	this.state = {
+	    history: [{board: board}]
+	}
+    }
+
+    handleClick(x, y) {
+	//alert(x + ":" + y);
+	const history = this.state.history;
+	const current = history[history.length - 1];
+	const board = current.board.splice(0);
+	board[x-1][y-1].value = 'x';
+
+	this.setState(
+	    {
+		history: history.concat([{board: board}])
+	    }
+	);
+
+    }
+
+    renderSquare(square_rec) {
+	const x = square_rec.x;
+	const y = square_rec.y;
+	return (
+	    <Square
+	    key={square_rec.key}
+	    value={square_rec.value}
+	    onClick={() => this.handleClick(x, y)}
+		/>)
+    }
+
     render() {
 	let rows = []
 	let value = null;
 	let isIndicated = null;
-	
+
+	const history = this.state.history;
+	const current = history[history.length-1];
 	for (let y = 1; y <= BOARD_HEIGHT; y++) {
-	    value = null;
-	    // only put pieces on these rows
-	    if ([1,2, 7, 8].includes(y)) {
-		value = 'P';
-	    }
 	    let squares = []
 	    for (let x = 1; x <= BOARD_WIDTH; x++) {
-		isIndicated = false;
-		if (x == 6 && y == 6) {
-		    isIndicated = true;
-		}
-		squares.push(<Square value={value} isIndicated={isIndicated} />)
+		const square_rec = current.board[x-1][y-1];
+		squares.push(
+		    this.renderSquare(square_rec)
+		)
 	    }
 	    rows.push(squares)
 	}
@@ -47,21 +92,20 @@ class Board extends React.Component {
 }
 
 class Square extends React.Component {
-    constructor(props) {
-	super(props);
-	this.value = props.value;
-	this.isIndicated = props.isIndicated
-    }
+
     render() {
-	const className = this.isIndicated ? 'dot active' : 'dot'
-	
+	//const className = this.state.isIndicated ? 'dot active' : 'dot'
+	const className = 'dot';
+	// const key = this.props.x + ':' + this.props.y;
 	return (
-		<div className='square square-indicator'>
-		{this.value}
-	        <span className={className} />
-	        </div>);
+		<div
+		   className='square' // square-indicator'
+		   onClick={this.props.onClick}
+		>
+		   {this.props.value}
+		   <span className={className} />
+		</div>);
     }
 }
 
 export default Game;
-
